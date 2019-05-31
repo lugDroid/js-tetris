a971c6f7c4174158503fbee1617bf1bf418de972
lugDroid <diazbastos@gmail.com>
2019-05-31 09:07:04 +0200

    Rotate functionality fixed

--- js/main.js
+++ js/main.js
@@ -17,38 +17,92 @@ class zPiece {
   }
 
   moveRight() {
-    if ((this.x + this.layout.length) <= this.size) {
+    if ((this.x + this.getMostRightPos()) < this.size) {
       this.x++
     }
   }
 
   moveLeft() {
-    if (this.x > 0) {
+    if (this.x + this.getMostLeftPos(0) > 0) {
       this.x--
     }
   }
 
+  getMostRightPos() {
+    let pos = this.layout.reduce( (acc, row, idx) => {
+      let rowPos = row.reduce( (acc, cur, idx) => {
+        //console.log("acc = " + acc + "; cur = " + cur + ";idx = " + idx)
+        if (cur == 1 && idx > acc) {
+          return idx
+        } else {
+          return acc
+        }
+      }, 0)
+
+      if (rowPos > acc) {
+        return rowPos
+      } else {
+        return acc
+      }
+    },0)
+
+    // pos is array index value
+    return pos + 1
+  }
+
+  getMostLeftPos() {
+    let pos = this.layout.reduce( (acc, row, idx) => {
+      let rowPos = row.reduce( (acc, cur, idx) => {
+        //console.log("acc = " + acc + "; cur = " + cur + ";idx = " + idx)
+        if (cur == 1 && idx < acc) {
+          return idx
+        } else {
+          return acc
+        }
+      }, 0)
+
+      if (rowPos < acc) {
+        return rowPos
+      } else {
+        return acc
+      }
+    },0)
+
+    // pos is array index value
+    console.log(pos)
+    return pos
+  }
+
   rotateLeft() {
+    // make copy of current layout
     let oldLayout = this.layout.map( row => {
       return row.slice()
     })
 
+    // rotate
     for (let i = 0; i < this.layout.length; i++) {
       for (let j = 0; j < this.layout.length; j++) {
-        this.layout[i][j] = oldLayout[this.size - j - 1][i]
+        this.layout[i][j] = oldLayout[this.layout.length - j - 1][i]
       }
     }
   }
 
   rotateRight() {
+    // make copy of current layout
     let oldLayout = this.layout.map( row => {
       return row.slice()
     })
 
+    // rotate
     for (let i = 0; i < this.layout.length; i++) {
       for (let j = 0; j < this.layout.length; j++) {
-        this.layout[i][j] = oldLayout[j][this.size - i - 1]
+        this.layout[i][j] = oldLayout[j][this.layout.length - i - 1]
       }
+    }
+
+    // ensure piece doesn't go off limits
+    if ((this.x + this.layout.length) > this.size) {
+      this.x--
     }
   }
 
@@ -58,7 +112,7 @@ class zPiece {
         // draw square
         if (this.layout[i][j] == 1) {
           ctx.beginPath()
-          ctx.rect((this.x + i) * this.size, (this.y + j) * this.size, this.size, this.size)
+          ctx.rect((this.x + j) * this.size, (this.y + i) * this.size, this.size, this.size)
           ctx.fillStyle = '#FF0000'
           ctx.fill()
           ctx.closePath()
@@ -75,12 +129,26 @@ class zPiece {
 
 }
 
-//let piece = new ITetromino(40, 40)
 let piece = new zPiece()
 
-//piece.log()
-//piece.rotateRight()
-//piece.log()
+document.addEventListener('keydown', function(event) {
+  // left arrow
+  if (event.keyCode == 37) {
+      piece.moveLeft()
+  }
+  // right arrow
+  else if (event.keyCode == 39) {
+      piece.moveRight()
+  }
+  // z key
+  else if (event.keyCode == 90) {
+    piece.rotateLeft()
+  }
+  // x key
+  else if (event.keyCode == 88) {
+    piece.rotateRight()
+  }
+})
 
 function draw() {
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.height)
@@ -89,13 +157,3 @@ function draw() {
 
 setInterval(draw, 10)
 
-document.addEventListener('keydown', function(event) {
-  // left arrow
-  if(event.keyCode == 37) {
-      piece.moveLeft()
-  }
-  // right arrow
-  else if(event.keyCode == 39) {
-      piece.moveRight()
-  }
-});
