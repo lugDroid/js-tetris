fa7fcdae4f07ae041d59fa15e9fdfb7f6189c96b
lugDroid <diazbastos@gmail.com>
2019-06-03 08:54:21 +0200

    Code splitted in files

--- js/zPiece.js
+++ js/zPiece.js
@@ -1,24 +1,126 @@
-export default class zPiece {
+//export default 
+
+class zPiece {
 
   constructor() {
-    this.layout = [[1,1,0],
-                   [0,1,1],
-                   [0,0,0]]
-    this.size = 3
+    this.layout = [
+      [1, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0]
+    ]
+    this.size = 20
+    this.x = 9
+    this.y = 0
+  }
+
+  moveRight() {
+    if ((this.x + this.getMostRightPos()) < this.size) {
+      this.x++
+    }
+  }
+
+  moveLeft() {
+    if (this.x + this.getMostLeftPos(0) > 0) {
+      this.x--
+    }
+  }
+
+  getMostRightPos() {
+    let pos = this.layout.reduce( (acc, row, idx) => {
+      let rowPos = row.reduce( (acc, cur, idx) => {
+        //console.log("acc = " + acc + "; cur = " + cur + ";idx = " + idx)
+        if (cur == 1 && idx > acc) {
+          return idx
+        } else {
+          return acc
+        }
+      }, 0)
+
+      if (rowPos > acc) {
+        return rowPos
+      } else {
+        return acc
+      }
+    },0)
+
+    // pos is array index value
+    return pos + 1
+  }
+
+  getMostLeftPos() {
+    let pos = this.layout.reduce( (acc, row, idx) => {
+      let rowPos = row.reduce( (acc, cur, idx) => {
+        //console.log("acc = " + acc + "; cur = " + cur + ";idx = " + idx)
+        if (cur == 1 && acc == -1) {
+          return idx
+        } else {
+          return acc
+        }
+      }, -1)
+
+      if (rowPos < acc && rowPos >= 0) {
+        return rowPos
+      } else {
+        return acc
+      }
+    },99)
+
+    // pos is array index value
+    return pos
   }
 
   rotateRight() {
-    let oldLayout = this.layout
+    // make copy of current layout
+    let oldLayout = this.layout.map( row => {
+      return row.slice()
+    })
 
-    for (i = 0; i < 3; i++) {
-      for (j = 0; j < 3; j++) {
-        this.layout[i][j] = oldLayout[this.size - j][i]
+    // rotate
+    for (let i = 0; i < this.layout.length; i++) {
+      for (let j = 0; j < this.layout.length; j++) {
+        this.layout[i][j] = oldLayout[this.layout.length - j - 1][i]
+      }
+    }
+
+    // ensure piece doesn't go off limits
+    if ((this.x + this.layout.length) > this.size) {
+      this.x--
+    }
+  }
+
+  rotateLeft() {
+    // make copy of current layout
+    let oldLayout = this.layout.map( row => {
+      return row.slice()
+    })
+
+    // rotate
+    for (let i = 0; i < this.layout.length; i++) {
+      for (let j = 0; j < this.layout.length; j++) {
+        this.layout[i][j] = oldLayout[j][this.layout.length - i - 1]
       }
     }
   }
 
-  getLayout() {
-    return this.layout
+  draw(ctx) {
+    for (let i = 0; i < this.layout.length; i++) {
+      for (let j = 0; j < this.layout.length; j++) {
+        // draw square
+        if (this.layout[i][j] == 1) {
+          ctx.beginPath()
+          ctx.rect((this.x + j) * this.size, (this.y + i) * this.size, this.size, this.size)
+          ctx.fillStyle = '#FF0000'
+          ctx.fill()
+          ctx.closePath()
+        }
+      }
+    }
+  }
+
+  log() {
+    this.layout.forEach( row => {
+      row.forEach( el => console.log(el))
+    })
   }
 
 }
