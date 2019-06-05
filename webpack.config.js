d1d1a7a4e17220a6685d5d79d8a84a7b144f9cc5
lugDroid <diazbastos@gmail.com>
2019-06-05 10:25:52 +0200

    Webpack dev server config added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
